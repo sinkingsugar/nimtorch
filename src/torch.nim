5f3c740fb9397187d68ebb76983433901972dc14
Giovanni <sinkingsugar@gmail.com>
2018-07-26 03:35:36 +0200

    first commit, started implementing torch tensors

---
+++ src/torch.nim
@@ -0,0 +1,74 @@
+include torch_cpp
+import macros, sequtils
+
+type
+  torch* = pointer
+  Tensor* = ATensor
+
+proc toIntListType(x: int): ilsize {.inline.} = x.ilsize
+
+proc zeros*(_: typedesc[torch], size: varargs[int, toIntListType]): Tensor {.inline.} =
+  let shape = cppinit(IntList, cast[ptr ilsize](unsafeAddr(size)), size.len.csize)
+  when defined cuda:
+    result = ACUDA().dynamicCppCall(zeros, shape)
+  else:
+    result = ACPU().dynamicCppCall(zeros, shape)
+
+iterator lenIter[T](s: openarray[T]): int {.inline.} =
+  ## Inline iterator on any-depth seq or array
+  ## Returns values in order
+  yield s.len
+  for item in s:
+    when item is array|seq:
+      for subitem in lenIter(item):
+        yield subitem
+      break
+
+iterator flatIter[T](s: openarray[T]): float32 {.inline.} =
+  ## Inline iterator on any-depth seq or array
+  ## Returns values in order
+  for item in s:
+    when item is array|seq:
+      for subitem in flatIter(item):
+        yield subitem.float32
+    else:
+      yield item.float32
+
+proc tensor*(_: typedesc[torch], data: openarray): Tensor {.inline.} =
+  # figure out size of array/seq
+  var size = newSeq[ilsize]()
+  for length in lenIter(data):
+    size.add(length)
+  
+  # make shape out of size
+  let shape = cppinit(IntList, cast[ptr ilsize](addr(size[0])), size.len.csize) 
+  
+  # flatten and eventually cast data
+  var flatData = toSeq(flatIter(data))
+  
+  # finally write into a tensor
+  when defined cuda:
+    result = ACUDA().dynamicCppCall(tensorFromBlob, addr(flatData[0]), shape).to(ATensor)
+  else:
+    result = ACPU().dynamicCppCall(tensorFromBlob, addr(flatData[0]), shape).to(ATensor)
+  
+when isMainModule:
+  var
+    z = torch.zeros(2, 1, 4)
+    data = [
+      [
+        [ 0.1,  0.2,  0.3,  0.4],
+        [-0.1, -0.2, -0.3, -0.4],
+        [ 0.5,  0.6,  0.7,  0.8]
+      ],
+      [
+        [ 0.1,  0.2,  0.3,  0.4],
+        [-0.1, -0.2, -0.3, -0.4],
+        [ 0.5,  0.6,  0.7,  0.8]
+      ]
+    ]
+    x = torch.tensor(data)
+  
+  z.printTensor()
+  echo ""
+  x.printTensor()
---
+++ src/torch_cpp.nim
@@ -0,0 +1,30 @@
+import ../deps/fragments/core/ffi/cpp
+
+# when defined(linux) and not defined(nimsuggest):
+#   static:
+#     echo "Checking if aten folder is in artifacts and populated"
+#     var check = gorgeEx("ls ../artifacts/aten/lib")
+#     if check.exitCode != 0:
+#       check = gorgeEx("""mkdir -p ../artifacts/aten && \
+# cd ../artifacts/aten && \
+# emconfigure cmake -DAT_LINK_STYLE=STATIC -DCAFFE2_CMAKE_BUILDING_WITH_MAIN_REPO=OFF -DCMAKE_C_FLAGS="-Wno-implicit-function-declaration \
+# -DEMSCRIPTEN -s DISABLE_EXCEPTION_CATCHING=0" -DCMAKE_CXX_FLAGS="-Wno-implicit-function-declaration -DEMSCRIPTEN -s DISABLE_EXCEPTION_CATCHING=0" \
+# -DCMAKE_INSTALL_PREFIX=/home/sugar/aten-wasm ../../deps/pytorch/aten""")
+#       doAssert(check.exitCode == 0, check.output)
+
+defineCppType(ATensor, "at::Tensor", "ATen/ATen.h")
+defineCppType(IntList, "at::IntList", "ATen/ATen.h")
+defineCppTYpe(ATensors, "std::vector<at::Tensor>", "vector")
+
+proc ACPU(): CppProxy {.importcpp: "at::CPU(at::kFloat)".}
+proc ACUDA(): CppProxy {.importcpp: "at::CUDA(at::kFloat)".}
+proc printTensor(t: var ATensor) {.importcpp: "at::print(#)".}
+
+{.passC: "-I../aten/include".}
+
+when defined wasm:
+  type ilsize = clonglong
+  {.passL: "../pytorch/aten/built/src/ATen/libATen_cpu.a ../pytorch/aten/built/confu-deps/cpuinfo/libcpuinfo.a".}
+else:
+  {.passL: "-L../aten/lib -lATen_cpu".}
+  type ilsize = clong
