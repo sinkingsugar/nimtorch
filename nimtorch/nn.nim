9e6e79741ff8e7a572e4110401081d811fc64289
Giovanni <sinkingsugar@gmail.com>
2018-09-09 18:46:34 +0900

    added Bilinear

--- nimtorch/nn.nim
+++ nimtorch/nn.nim
@@ -8,7 +8,7 @@ import math
 
 type
   Module = object of RootObj
-    forward: proc(input: Tensor): Tensor
+    forward: proc(input: varargs[Tensor]): Tensor
 
   LinearModule = object of Module
     in_features: int
@@ -16,7 +16,10 @@ type
     weight: Tensor
     bias: Tensor
 
-proc `()`*(m: Module; input: Tensor): Tensor {.inline.} = m.forward(input)
+  BilinearModule = object of LinearModule
+    in2_features: int
+
+proc `()`*(m: Module; input: varargs[Tensor]): Tensor {.inline.} = m.forward(input)
 
 proc reset_parameters*(m: var LinearModule) =
   m.weight = init.kaiming_uniform(m.weight, a = math.sqrt(5.float))
@@ -37,12 +40,41 @@ proc Linear*(in_features, out_features: int; bias: bool = true): LinearModule =
   if bias:
     m.bias = torch.zeros(@[out_features])
   
-  m.forward = proc (input: Tensor): Tensor =
-    let step1 = input.matmul(m.weight.t())
+  m.forward = proc (input: varargs[Tensor]): Tensor =
+    let step1 = input[0].matmul(m.weight.t())
     if not m.bias.isNil:
       return step1 + m.bias
     else:
       return step1
+
+  m.reset_parameters()
+  
+  return m
+
+proc reset_parameters*(m: var BilinearModule) =
+  let bound = 1 / math.sqrt(m.weight.size(1).float)
+  
+  m.weight = init.uniform(m.weight, -bound, bound)
+
+  if not m.bias.isNil:
+    m.bias = init.uniform(m.bias, -bound, bound)
+
+proc Bilinear*(in1_features, in2_features, out_features: int; bias: bool = true): BilinearModule =
+  var m: BilinearModule
+  
+  m.in_features = in1_features
+  m.in2_features = in2_features
+  m.out_features = out_features
+  
+  m.weight = torch.zeros(@[out_features, in1_features, in2_features])
+  if bias:
+    m.bias = torch.zeros(@[out_features])
+  else:
+    var emptyList: IntList
+    m.bias = torch.zeros(emptyList)
+  
+  m.forward = proc (input: varargs[Tensor]): Tensor =
+    return torch.bilinear(input[0], input[1], m.weight, m.bias)
 
   m.reset_parameters()
   
