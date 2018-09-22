e1a01b60581083f6e1f3da32954333e43ff6f6bf
Jörg Wollenschläger <joerg.wollenschlaeger@gmail.com>
2018-09-22 12:07:45 +0900

    Use working network topo

--- tests/test_xor.nim
+++ tests/test_xor.nim
@@ -16,14 +16,15 @@ let targets = torch.tensor([
 ])
 
 let
-  fc = nn.Linear(2, 1)
+  fc1 = nn.Linear(2, 4)
+  fc2 = nn.Linear(4, 1)
   loss_fn = nn.MSELoss()
-  optimizer = optim.SGD(fc.parameters(), lr = 0.001, momentum = 0.1)
+  optimizer = optim.SGD(fc1.parameters & fc2.parameters , lr = 0.01, momentum = 0.1)
 
 for i in 0 ..< 50000:
   optimizer.zero_grad()
 
-  let predictions = fc(inputs).sin()
+  let predictions = inputs.fc1.relu.fc2.sigmoid
 
   let loss = loss_fn(predictions, targets)
   loss.backward()
--- tests/test_xor.py
+++ tests/test_xor.py
@@ -16,14 +16,16 @@ targets = torch.tensor([
   [0.0],
 ])
 
-fc = nn.Linear(2, 1)
+fc1 = nn.Linear(2, 4)
+fc2 = nn.Linear(4, 1)
 loss_fn = nn.MSELoss()
-optimizer = optim.SGD(fc.parameters(), lr = 0.01, momentum = 0.1)
+optimizer = optim.SGD(list(fc1.parameters()) + list(fc2.parameters()), lr = 0.01, momentum = 0.1)
 
 for i in range(0, 50000):
   optimizer.zero_grad()
 
-  predictions = fc(inputs).sin()
+  predictions = fc1(inputs).relu()
+  predictions = fc2(predictions).sigmoid()
 
   loss = loss_fn(predictions, targets)
   loss.backward()
