74e1a57d81818bbd64421af340fbbe72dfe34b2c
Giovanni <sinkingsugar@gmail.com>
2018-10-07 21:04:18 +0900

    add automatic aten conda uploads and build

---
+++ conda/aten/build.sh
@@ -0,0 +1,6 @@
+ls $ATEN
+mkdir -p $PREFIX/etc/conda/activate.d
+mkdir -p $PREFIX/etc/conda/deactivate.d
+echo "export ATEN=$PREFIX" >> $PREFIX/etc/conda/activate.d/env_vars.sh
+echo "unset ATEN" >> $PREFIX/etc/conda/deactivate.d/env_vars.sh
+cp -r $ATEN/* $PREFIX/
