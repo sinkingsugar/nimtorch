31cab9919179ec103f7d3dec5aa83278075a7cac
Giovanni <sinkingsugar@gmail.com>
2018-10-09 07:03:39 +0900

    added aten as nimtorch conda requirement

--- conda/nimtorch/build.sh
+++ conda/nimtorch/build.sh
@@ -1,9 +1,3 @@
-ls $ATEN
-mkdir -p $PREFIX/etc/conda/activate.d
-mkdir -p $PREFIX/etc/conda/deactivate.d
-echo "export ATEN=$PREFIX" >> $PREFIX/etc/conda/activate.d/env_vars.sh
-echo "unset ATEN" >> $PREFIX/etc/conda/deactivate.d/env_vars.sh
-cp -r $ATEN/* $PREFIX/
 mkdir -p $PREFIX/dist/nimtorch
 cp -r * $PREFIX/dist/nimtorch/
 cp bin/.nimtorch-post-link.sh $PREFIX/bin/
