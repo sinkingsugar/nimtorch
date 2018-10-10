413d445334d342945edca7cabd59f22d6b333cd5
Jörg Wollenschläger <joerg.wollenschlaeger@gmail.com>
2018-10-10 11:54:52 +0900

    Fix pointer constness issue in clang

--- torch/tensors.nim
+++ torch/tensors.nim
@@ -100,7 +100,7 @@ iterator items*(ints: AIntList): AInt64 {.inline.} =
     yield ints[i].to(AInt64)
 
 proc toIntList(self: AIntList): IntList =
-  proc unsafeAddr(self: CppProxy): pointer {.importcpp: "&(#)".}
+  proc unsafeAddr(self: CppProxy): pointer {.importcpp: "(void*)&(#)".}
   result.setLen(self.len)
   when sizeof(int) == sizeof(int64):
     copyMem(addr result[0], unsafeAddr(self[0]), result.len * sizeof(int64))
