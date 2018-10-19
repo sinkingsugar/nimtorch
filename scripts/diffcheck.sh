019ee1dcacc05e609e2eea5449a86400fd93d4e0
Giovanni <sinkingsugar@gmail.com>
2018-10-19 10:44:23 +0900

    temporary fix diffcheck script

--- scripts/diffcheck.sh
+++ scripts/diffcheck.sh
@@ -1,6 +1,9 @@
+DATE=`date`
 # Notify if we have changes in declarations
-export DIFF=`git diff --exit-code torch/declarations.nim`
-test -z "$DIFF" || curl -X POST -H 'Content-Type: application/json' -d "{\"value1\":\"Changes in declarations.nim\",\"value2\":\"$CI_COMMIT_SHA\",\"value3\":\"$DIFF\"}" https://maker.ifttt.com/trigger/trello_works_notify/with/key/$IFTTT_KEY && true
+DIFF=`git diff --exit-code torch/declarations.nim`
+MSG="Changes in declarations.nim, $DATE"
+test -z "$DIFF" || curl -X POST -H 'Content-Type: application/json' -d '{"value1":"'"$MSG"'","value2":"'"$CI_COMMIT_SHA"'","value3":""}' https://maker.ifttt.com/trigger/trello_works_notify/with/key/$IFTTT_KEY && true
 # Notify if we have changes in derivatives
-export DIFF=`git diff --exit-code torch/derivatives.nim`
-test -z "$DIFF" || curl -X POST -H 'Content-Type: application/json' -d "{\"value1\":\"Changes in derivatives.nim\",\"value2\":\"$CI_COMMIT_SHA\",\"value3\":\"$DIFF\"}" https://maker.ifttt.com/trigger/trello_works_notify/with/key/$IFTTT_KEY && true
+DIFF=`git diff --exit-code torch/derivatives.nim`
+MSG="Changes in derivatives.nim, $DATE"
+test -z "$DIFF" || curl -X POST -H 'Content-Type: application/json' -d '{"value1":"'"$MSG"'","value2":"'"$CI_COMMIT_SHA"'","value3":""}' https://maker.ifttt.com/trigger/trello_works_notify/with/key/$IFTTT_KEY && true
