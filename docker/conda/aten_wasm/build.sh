b4dd4f68696fca9d930e76591c566c854a97db06
Giovanni <sinkingsugar@gmail.com>
2018-10-21 17:33:43 +0900

    minor renaming of conda env scripts for aten

--- docker/conda/aten_wasm/build.sh
+++ docker/conda/aten_wasm/build.sh
@@ -1,5 +1,5 @@
 mkdir -p $PREFIX/etc/conda/activate.d
 mkdir -p $PREFIX/etc/conda/deactivate.d
-echo "export ATEN=$PREFIX" >> $PREFIX/etc/conda/activate.d/env_vars.sh
-echo "unset ATEN" >> $PREFIX/etc/conda/deactivate.d/env_vars.sh
+echo "export ATEN=$PREFIX" >> $PREFIX/etc/conda/activate.d/aten-env_vars.sh
+echo "unset ATEN" >> $PREFIX/etc/conda/deactivate.d/aten-env_vars.sh
 cp -r $ATEN/* $PREFIX/
