10d801e1d4918e9fd639bfcdd8f3b2dde09c9f1c
Giovanni <sinkingsugar@gmail.com>
2018-10-22 16:56:01 +0900

    add iomp hack in build.sh, disabled for now tho

--- docker/conda/aten/build.sh
+++ docker/conda/aten/build.sh
@@ -1,5 +1,8 @@
 mkdir -p $PREFIX/etc/conda/activate.d
 mkdir -p $PREFIX/etc/conda/deactivate.d
-echo "export ATEN=$PREFIX" >> $PREFIX/etc/conda/activate.d/aten-env_vars.sh
-echo "unset ATEN" >> $PREFIX/etc/conda/deactivate.d/aten-env_vars.sh
+echo 'export ATEN="'"$PREFIX"'"' >> "$PREFIX/etc/conda/activate.d/aten-env_vars.sh"
+# echo 'export IOMP5_PATH="'"$PREFIX/lib/libiomp5.so"'"' >> "$PREFIX/etc/conda/activate.d/aten-env_vars.sh"
+# echo 'export LD_PRELOAD="'"$LD_PRELOAD:$IOMP5_PATH"'"' >> "$PREFIX/etc/conda/activate.d/aten-env_vars.sh"
+echo "unset ATEN" >> "$PREFIX/etc/conda/deactivate.d/aten-env_vars.sh"
+# echo "unset IOMP5_PATH" >> "$PREFIX/etc/conda/deactivate.d/aten-env_vars.sh"
 cp -r $ATEN/* $PREFIX/
