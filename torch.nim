ef7630ad01801056d13bf8ae7139e962f1e07434
Giovanni <sinkingsugar@gmail.com>
2018-11-07 19:10:06 +0900

    Revert "renamed wasm define into webassembly to fork from nim  define"
    This reverts commit adaa47eb88fba5b358278029d0f46a4baed41c7d.

--- torch.nim
+++ torch.nim
@@ -125,7 +125,7 @@ when isMainModule:
   
   hy.print()
 
-  when not defined webassembly:
+  when not defined wasm:
     var hycopy = hy.copy()
 
     var longt = zeros(@[1, 1, 1], dtype = LongTensor)
