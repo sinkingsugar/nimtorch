cd8f58ae6f63e75dcd5438b9ade54f98e983466e
Giovanni <sinkingsugar@gmail.com>
2018-12-01 15:38:14 +0100

    inject config.nims env wide when installing using conda

--- conda/nimtorch/bld.bat
+++ conda/nimtorch/bld.bat
@@ -1,2 +1,3 @@
 mkdir %PREFIX%\dist\pkgs\nimtorch-#head
 xcopy . %PREFIX%\dist\pkgs\nimtorch-#head /s /e
+copy config.nims %PREFIX%\config\
--- conda/nimtorch/build.sh
+++ conda/nimtorch/build.sh
@@ -1,2 +1,3 @@
 mkdir -p $PREFIX/dist/pkgs/nimtorch-\#head
 cp -r * $PREFIX/dist/pkgs/nimtorch-\#head/
+cp config.nims $PREFIX/config/
