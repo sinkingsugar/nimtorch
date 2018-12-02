751e43ecf3e8a56f719abdce9e31792b099d28ac
Giovanni <sinkingsugar@gmail.com>
2018-12-02 16:35:49 +0100

    make sure to create config directory when building conda package

--- conda/nimtorch/bld.bat
+++ conda/nimtorch/bld.bat
@@ -1,3 +1,4 @@
 mkdir %PREFIX%\dist\pkgs\nimtorch-#head
 xcopy . %PREFIX%\dist\pkgs\nimtorch-#head /s /e
+mkdir %PREFIX%\config
 copy config.nims %PREFIX%\config\
--- conda/nimtorch/build.sh
+++ conda/nimtorch/build.sh
@@ -1,3 +1,4 @@
 mkdir -p $PREFIX/dist/pkgs/nimtorch-\#head
 cp -r * $PREFIX/dist/pkgs/nimtorch-\#head/
+mkdir -p $PREFIX/config
 cp config.nims $PREFIX/config/
