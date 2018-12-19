0c38414d8ba79c9632918d230a93c948006824c5
Giovanni <sinkingsugar@gmail.com>
2018-12-19 21:05:36 +0100

    conda fixes

--- docker/conda/aten/bld.bat
+++ docker/conda/aten/bld.bat
@@ -1,7 +1,7 @@
 mkdir %PREFIX%\etc\conda\activate.d
 mkdir %PREFIX%\etc\conda\deactivate.d
 @echo set ATEN=%PREFIX%>> %PREFIX%\etc\conda\activate.d\aten-env_vars.bat
-@echo set ATEN_LIBS=%PREFIX%\lib>> %PREFIX%\etc\conda\activate.d\aten-env_vars.bat
+@echo set ATEN_LIBS=%PREFIX%/lib>> %PREFIX%\etc\conda\activate.d\aten-env_vars.bat
 @echo set PATH=%PATH%;%%ATEN_LIBS%%>> %PREFIX%\etc\conda\activate.d\aten-env_vars.bat
 @echo set ATEN>> %PREFIX%\etc\conda\deactivate.d\aten-env_vars.bat
 @echo set ATEN_LIBS>> %PREFIX%\etc\conda\deactivate.d\aten-env_vars.bat
